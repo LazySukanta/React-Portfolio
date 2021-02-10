f901e90e056d15fc40bd1d14a0bc3f48ed5bc0e5
Sukanta <dindasukanta19@gmail.com>
2021-02-11 00:32:07 +0530

    Updates

---
+++ precache-manifest.28bb6fa94899650fe69293a8210e1eec.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69bc7104781941d970b64c3248e31ce2",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "19e0640c871c71bd56db",
+    "url": "/React-Portfolio/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "49b31e7b08687340a6c9",
+    "url": "/React-Portfolio/static/css/main.2c89c7e7.chunk.css"
+  },
+  {
+    "revision": "19e0640c871c71bd56db",
+    "url": "/React-Portfolio/static/js/2.3be9bea7.chunk.js"
+  },
+  {
+    "revision": "402ef71888331977f0b109e60b155b64",
+    "url": "/React-Portfolio/static/js/2.3be9bea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b31e7b08687340a6c9",
+    "url": "/React-Portfolio/static/js/main.079f25a0.chunk.js"
+  },
+  {
+    "revision": "56993ab3d4c34264a97e",
+    "url": "/React-Portfolio/static/js/runtime-main.a571859c.js"
+  },
+  {
+    "revision": "a568565ff8ebe6e205133f875d04f5b5",
+    "url": "/React-Portfolio/static/media/MyImg1.a568565f.jpg"
+  },
+  {
+    "revision": "fbd16cabdf31130099b19cd6255ae1da",
+    "url": "/React-Portfolio/static/media/Weather.fbd16cab.png"
+  },
+  {
+    "revision": "4a4b7ddc1f1b9a5cf56a088873c0295a",
+    "url": "/React-Portfolio/static/media/meme.4a4b7ddc.jpg"
+  },
+  {
+    "revision": "bc1332752122109eca8ed4c4aeff4e13",
+    "url": "/React-Portfolio/static/media/notes.bc133275.png"
+  },
+  {
+    "revision": "ecadfc34088f09afed0a58d5c2dc5c87",
+    "url": "/React-Portfolio/static/media/ticTac.ecadfc34.png"
+  }
+]);
