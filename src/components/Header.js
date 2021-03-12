da8950a4940e2fa7c13f57787ccf54210feb1f04
Sukanta <dindasukanta19@gmail.com>
2021-03-12 11:29:09 +0530

    Resume link Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -81,7 +81,7 @@ const Header = () => {
             </ul>
             <Button className="btns draw-border resume" style={{}}>
               <a
-                href="https://docs.google.com/document/d/1mwspRv0pmqgu0TTUrmWrk4W_XXU5FXfZay6e21vQog0/edit?usp=sharing"
+                href="https://drive.google.com/file/d/1WX6PpTbAP7_S2mlK4Tl6ktNoHfhWUpow/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
               >
